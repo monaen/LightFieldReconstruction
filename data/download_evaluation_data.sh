7765501bbb50569b84bf46bde9c3be67bb0c3626
naenmong <naen.mong@gmail.com>
2020-09-22 09:44:27 +0800

    update

--- data/download_evaluation_data.sh
+++ data/download_evaluation_data.sh
@@ -9,24 +9,28 @@
 # |              title={High-dimensional dense residual convolutional neural network for light field reconstruction},  | #
 # |              author={Meng, Nan and So, Hayden Kwok-Hay and Sun, Xing and Lam, Edmund},                             | #
 # |              journal={IEEE Transactions on Pattern Analysis and Machine Intelligence},                             | #
-# |              year={2019},                                                                                          | #
-# |              publisher={IEEE}                                                                                      | #
+# |              year={2019}                                                                                           | #
 # |             }                                                                                                      | #
-# |                                                                                                                    | #
-# | Paper:                                                                                                             | #
-# |     High-Dimensional Dense Residual Convolutional Neural Network for Light Field Reconstruction                    | #
-# |     Nan Meng, Hayden Kwok-Hay So, Xing Sun, and Edmund Y. Lam                                                      | #
-# |     IEEE Transactions on Pattern Analysis and Machine Intelligence, 2019                                           | #
+# |     @article{meng2020high,                                                                                         | #
+# |              title={High-order residual network for light field super-resolution},                                 | #
+# |              author={Meng, Nan and Wu, xiaofei and Liu, Jianzhuang and Lam, Edmund},                               | #
+# |              journal={Association for the Advancement of Artificial Intelligence},                                 | #
+# |              volume={34},                                                                                          | #
+# |              number={7},                                                                                           | #
+# |              pages={11757-11764},                                                                                  | #
+# |              month={February},                                                                                     | #
+# |              year={2020},                                                                                          | #
+# |             }                                                                                                      | #
 # |                                                                                                                    | #
 # | Contact:                                                                                                           | #
 # |     author: Nan Meng                                                                                               | #
 # |     email:  u3003637@connect.hku.hk  |   nanmeng.uestc@hotmail.com                                                 | #
 # ====================================================================================================================== #
 
-# This script downloads the pretrained model of HDDRNet for Sx2 task and unzips it.
+# This script downloads the sample light field data for evaluation.
 # DIR = "$( cd "$(dirname "$0")" ; pwd -P )"
 
-echo "Downloading the sample light field data of evaluation ........"
+echo "Downloading the sample light field data for evaluation ........"
 
 wget --no-check-certificate https://github.com/monaen/LightFieldReconstruction/raw/storage/data/evaluation/buddha.mat
 
