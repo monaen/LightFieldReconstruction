95803e509b8181527b53590bf8c8013f533b5469
naenmong <naen.mong@gmail.com>
2020-09-22 09:59:16 +0800

    update occlusions20

--- data/download_occlusions20.sh
+++ data/download_occlusions20.sh
@@ -27,7 +27,7 @@
 # |     email:  u3003637@connect.hku.hk  |   nanmeng.uestc@hotmail.com                                                 | #
 # ====================================================================================================================== #
 
-# This script downloads the sample light field data for evaluation.
+# This script downloads the light field occlusions20 dataset for testing.
 # DIR = "$( cd "$(dirname "$0")" ; pwd -P )"
 
 echo "Downloading the sample light field data for evaluation ........"
@@ -53,23 +53,24 @@ wget --no-check-certificate https://github.com/monaen/LightFieldReconstruction/r
 wget --no-check-certificate https://github.com/monaen/LightFieldReconstruction/raw/storage/data/evaluation/occlusions20/occlusions_6.mat
 wget --no-check-certificate https://github.com/monaen/LightFieldReconstruction/raw/storage/data/evaluation/occlusions20/occlusions_9.mat
 
-mv occlusions_1.mat testset/occlusions_1.mat
-mv occlusions_12.mat testset/occlusions_12.mat
-mv occlusions_15.mat testset/occlusions_15.mat
-mv occlusions_18.mat testset/occlusions_18.mat
-mv occlusions_22.mat testset/occlusions_22.mat
-mv occlusions_24.mat testset/occlusions_24.mat
-mv occlusions_26.mat testset/occlusions_26.mat
-mv occlusions_27.mat testset/occlusions_27.mat
-mv occlusions_3.mat testset/occlusions_3.mat
-mv occlusions_30.mat testset/occlusions_30.mat
-mv occlusions_32.mat testset/occlusions_32.mat
-mv occlusions_36.mat testset/occlusions_36.mat
-mv occlusions_39.mat testset/occlusions_39.mat
-mv occlusions_42.mat testset/occlusions_42.mat
-mv occlusions_46.mat testset/occlusions_46.mat
-mv occlusions_48.mat testset/occlusions_48.mat
-mv occlusions_51.mat testset/occlusions_50.mat
-mv occlusions_50.mat testset/occlusions_51.mat
-mv occlusions_6.mat testset/occlusions_6.mat
-mv occlusions_9.mat testset/occlusions_9.mat
+mkdir testset/occlusions20
+mv occlusions_1.mat testset/occlusions20/occlusions_1.mat
+mv occlusions_12.mat testset/occlusions20/occlusions_12.mat
+mv occlusions_15.mat testset/occlusions20/occlusions_15.mat
+mv occlusions_18.mat testset/occlusions20/occlusions_18.mat
+mv occlusions_22.mat testset/occlusions20/occlusions_22.mat
+mv occlusions_24.mat testset/occlusions20/occlusions_24.mat
+mv occlusions_26.mat testset/occlusions20/occlusions_26.mat
+mv occlusions_27.mat testset/occlusions20/occlusions_27.mat
+mv occlusions_3.mat testset/occlusions20/occlusions_3.mat
+mv occlusions_30.mat testset/occlusions20/occlusions_30.mat
+mv occlusions_32.mat testset/occlusions20/occlusions_32.mat
+mv occlusions_36.mat testset/occlusions20/occlusions_36.mat
+mv occlusions_39.mat testset/occlusions20/occlusions_39.mat
+mv occlusions_42.mat testset/occlusions20/occlusions_42.mat
+mv occlusions_46.mat testset/occlusions20/occlusions_46.mat
+mv occlusions_48.mat testset/occlusions20/occlusions_48.mat
+mv occlusions_51.mat testset/occlusions20/occlusions_50.mat
+mv occlusions_50.mat testset/occlusions20/occlusions_51.mat
+mv occlusions_6.mat testset/occlusions20/occlusions_6.mat
+mv occlusions_9.mat testset/occlusions20/occlusions_9.mat
